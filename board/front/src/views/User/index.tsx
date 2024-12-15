cac66fcb7e46367150a0ad097eb032b0ebc23f93
Park-joehyun <joehyun000@naver.com>
2024-12-16 00:12:20 +0900

    캘린더 날짜오류 수정

--- board/front/src/views/User/index.tsx
+++ board/front/src/views/User/index.tsx
@@ -538,7 +538,7 @@ return (
                     const date = new Date(dateString);
                     const year = date.getFullYear();
                     const month = date.getMonth() + 1;
-                    const day = date.getDate();
+                    const day = date.getDate() + 1;
                     return `${year}년 ${month}월 ${day}일`;
                   };
 
